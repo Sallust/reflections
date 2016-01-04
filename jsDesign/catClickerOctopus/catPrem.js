183c85947270fe751e33a68f48d58090cda58447
Sallust <daniyom@gmail.com>
2016-01-04 15:26:12 -0500

    feat: Admin feature
    -add new admin features where new cats can be added to code

--- jsDesign/catClickerOctopus/catPrem.js
+++ jsDesign/catClickerOctopus/catPrem.js
@@ -2,6 +2,7 @@
 
 var model = {
 	displayedCat: null,
+	adminView: false,
 	catArray: [
 		"White Cat",
 		"Mad Cat",
@@ -21,6 +22,15 @@ var model = {
 			model.catData.push(obj);
 		};
 		//initialized the array, and cat objects in the array
+	},
+	addCat: function(newCat, newUrl,newClicks) {
+		var htmlSrc = "http://lorempixel.com/440/380/cats/" + newUrl;
+		var obj = {
+			name: newCat,
+			src: htmlSrc,
+			clicks: newClicks
+		}
+		model.catData.push(obj);
 	}
 };
 
@@ -44,17 +54,48 @@ var octopus = {
 		model.catData[currentCatIndex].clicks += 1;
 		catView.render();
 	},
+	openAdmin: function() {
+		model.adminView = true;
+		adminView.render();
+
+	},
+	updateAdmin: function() {
+		model.adminView = false;
+		console.log("sup");
+		adminView.capture();
+		adminView.hideView();
+	},
+	closeAdmin: function() {
+		model.adminView = false;
+		console.log("sup2");
+		adminView.hideView();
+	},
+	process: function(newCat, newUrl,newClicks) {
+		model.catArray.push(newCat);
+		buttonView.init();
+
+		model.addCat(newCat, newUrl,newClicks);
+
+		model.displayedCat = model.catData[model.catData.length - 1];
+
+		catView.render();
+		console.log(model.catData);
+
+	},
 
 	init: function () {
 		model.init();
 		model.displayedCat = model.catData[0];
 		buttonView.init();
 		catView.init();
+		adminView.init();
+
 	}
 };
 
 var buttonView = {
 	init: function () {
+		$("#buttonList").html("");
 		var catArray = octopus.returnCatArray();
 		for (var i = 0; i < catArray.length; i++) {
 			var buttonID = "button" + i;
@@ -68,13 +109,13 @@ var buttonView = {
 		};
 		//draw the buttons
 		//on click sends data to octopus
-	},
+	}
 
 };
 
 var catView = {
 	init: function() {
-		this.img = $("#image")
+		this.img = $("#image");
 		this.heading = $("#heading");
 		this.clickSpan = $("#clicked");
 		this.img.click(function() {
@@ -91,6 +132,50 @@ var catView = {
 		this.clickSpan.text(currentCatObj.clicks);
 	}
 };
+
+var adminView = {
+	init: function() {
+		this.admin = $("#admin");
+		this.catForm = $("#cat-form");
+		this.name = $("#name");
+		this.url = $("#url");
+		this.clicks = $("#clicks");
+		this.cancel = $("#cancel");
+		this.submit = $("#submit");
+
+
+
+		//admin click listener
+		this.admin.click(function () {
+			octopus.openAdmin();
+		});
+
+		//save click listener
+		this.submit.click(function() {
+			octopus.updateAdmin();
+		})
+		//cancel click listener
+		this.cancel.click(function() {
+			octopus.closeAdmin();
+		})
+
+	},
+	render: function() {
+		this.catForm.show();
+	},
+	hideView: function() {
+		this.name.val(""); this.url.val(""); this.clicks.val("");
+		this.catForm.hide();
+	},
+	capture: function() {
+		var newCat = this.name.val();
+		var newUrl = this.url.val();
+		var newClicks = parseInt(this.clicks.val(), 10);
+		octopus.process(newCat,newUrl,newClicks);
+	}
+
+
+}
 
 octopus.init();
 
