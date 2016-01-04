1a8153a7650839ff5135b24a46a86724b8b114db
Sallust <daniyom@gmail.com>
2016-01-04 13:01:07 -0500

    feat: improved Cat Clicker Premium (includes Udacity solution)
    -created model.displayedCat instead of cumbersome imgDisplay property
    -moved button array into model
    -fixed the resulting "cheat" by creating a returnCats function
    -moved some view variables into view by using this.variable

--- jsDesign/catClickerOctopus/catPrem.js
+++ jsDesign/catClickerOctopus/catPrem.js
@@ -1,59 +1,53 @@
 'use strict'
 
-
-var buttons = [
-			"White Cat",
-			"Mad Cat",
-			"Contempletive Cat",
-			"House Cat",
-			"Cute Cat"
-			]
-
 var model = {
-
+	displayedCat: null,
+	catArray: [
+		"White Cat",
+		"Mad Cat",
+		"Contempletive Cat",
+		"House Cat",
+		"Cute Cat"
+	],
 	catData: [],
 	init: function() {
-		for (var i = 0; i < buttons.length; i++) {
+		for (var i = 0; i < model.catArray.length; i++) {
 			var htmlSrc = "http://lorempixel.com/440/380/cats/" + i;
 			var obj = {
-				name: buttons[i],
+				name: model.catArray[i],
 				src: htmlSrc,
-				clicks: 0,
-				imgDisplay: false
+				clicks: 0
 			}
 			model.catData.push(obj);
 		};
-		model.catData[0].imgDisplay = true;
 		//initialized the array, and cat objects in the array
-		//initialize buttons
 	}
 };
 
 var octopus = {
+	//Solution more eloquently acheives this by setting a model.currentCat
 	changeImageStatus: function(buttonID) {
-		for (var i = 0; i < model.catData.length; i++) {
-			model.catData[i].imgDisplay = false;
-		};
-		model.catData[buttonID].imgDisplay = true;
+		model.displayedCat = model.catData[buttonID];
 		catView.render();
 	},
 
 	returnCat: function() {
-		for (var i = 0; i < model.catData.length; i++) {
-			if (model.catData[i].imgDisplay == true) {
-				return model.catData[i];
-			}
-		};
+		return model.displayedCat;
 	},
+
+	returnCatArray: function () {
+		return model.catArray;
+	},
+
 	addClick: function() {
 		var currentCatIndex = model.catData.indexOf(octopus.returnCat());
 		model.catData[currentCatIndex].clicks += 1;
 		catView.render();
-
 	},
 
 	init: function () {
 		model.init();
+		model.displayedCat = model.catData[0];
 		buttonView.init();
 		catView.init();
 	}
@@ -61,29 +55,29 @@ var octopus = {
 
 var buttonView = {
 	init: function () {
-		for (var i = 0; i < buttons.length; i++) {
+		var catArray = octopus.returnCatArray();
+		for (var i = 0; i < catArray.length; i++) {
 			var buttonID = "button" + i;
-			var newButton = $("<button id ='" + buttonID + "'>" + buttons[i] + "</button>");
-			
+			var newButton = $("<button id ='" + buttonID + "'>" + catArray[i] + "</button>");
 			newButton.click((function(i){
 				return function() {
 					octopus.changeImageStatus(i);
 				}
 			})(i));
 			$("#buttonList").append(newButton);
-			
 		};
 		//draw the buttons
-		//on click sends data to octopus 
+		//on click sends data to octopus
 	},
 
 };
 
 var catView = {
 	init: function() {
-
-		var img = $("#image");
-		img.click(function() {
+		this.img = $("#image")
+		this.heading = $("#heading");
+		this.clickSpan = $("#clicked");
+		this.img.click(function() {
 			octopus.addClick();
 		});
 
@@ -92,14 +86,9 @@ var catView = {
 
 	render: function() {
 		var currentCatObj = octopus.returnCat();
-
-		var heading = $("#heading");
-		var img = $("#image");
-		var clickSpan = $("#clicked");
-
-		heading.text(currentCatObj.name);
-		img.attr("src",currentCatObj.src);
-		clickSpan.text(currentCatObj.clicks);
+		this.heading.text(currentCatObj.name);
+		this.img.attr("src",currentCatObj.src);
+		this.clickSpan.text(currentCatObj.clicks);
 	}
 };
 
@@ -116,99 +105,5 @@ octopus.init();
 
 /*
 
-//$('.header').text('sup');
-var count = 0;
-
-var catArray = [
-	"White Cat",
-	"Mad Cat",
-	"Contempletive Cat",
-	"House Cat",
-	"Cute Cat"];
-
-var obj = {};
-
-
-
-for (var i = 0; i < catArray.length; i++) {
-
-	obj[catArray[i]]= 0;
-	
-
-
-	//make the div with the actual cat
-	var elem = $('<div style="display: none;" id="cat'+ i + '"><h2>'+ catArray[i] + '</h2></div>');
-	var img = '<img src="http://lorempixel.com/640/480/cats/'+ (i + 1)+'">'
-	
-	var clicked = "<h2>I've been clicked <span id='clicked'> 0 </span> times </h2>"
-	var clickSpan = $('#clicked')
-	$('#cats').append(elem.append(img,clicked));
-	//elem.click(function(e){
-		//obj
-	//})
-
-	elem.click((function(copy,spanCopy){
-		return function() {
-			obj[copy] += 1;
-			spanCopy.text('testText');
-
-		}
-	})(catArray[i],clickSpan));
-
-	elem.click((function(copy){
-		return function(){
-			copy.text(obj[catArray[i]]);
-		}
-	})(clickSpan))
-
-
-
-
-	//make the list on the left
-	var listElem = $('<li>'+catArray[i]+'</li>')
-	$('#list').append(listElem);
-	listElem.click((function(copy){
-		return function() {
-			copy.show();
-		}
-	})(elem))
-};
-
-function clickChange() {
-	$()
-
-}
-//$('#heading1').text(cat1);
-//$('#heading2').text(cat2);
-/*
-$('#cat1').click(function(e) {
-	count += 1
-	$('#clicked').text(count);
-
-})
-
-$('#cat2').click(function(e) {
-	count += 1
-	$('#clicked').text(count);
-
-})
-
-
-var nums = [1,2,3];
-for (var i = 0; i < nums.length; i++) {
-
-	var num = nums[i];
-	var elem = $('<p id="'+ num + '">'+ num + '</p>')
-	$('.closure').append(elem);
-
-	elem.click((function(numCopy){
-		return function () {
-			alert(numCopy)
-		};
-	})(num));
-
-
-
-};
 */
 
