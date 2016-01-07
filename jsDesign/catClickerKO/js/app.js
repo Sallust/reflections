8347a79cb82ffbae45a08ed89f7bd213f2ed9282
Sallust <daniyom@gmail.com>
2016-01-06 22:21:51 -0500

    refactor: getting ready to create multiple cats!
    -Cat now gets passed data so as to facilitate the creation of multiple cats
    -meow

--- jsDesign/catClickerKO/js/app.js
+++ jsDesign/catClickerKO/js/app.js
@@ -1,8 +1,8 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/tabby.jpg');
-	this.imgAttribution = ko.observable('http://www.flickr.com');
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
 
 	this.catLevelArray = ["infant","toddler", "pre-teen", "annoying teen", "teen", "adolescent", "adult", "dead...awkward. You totally killed it"]
 
@@ -10,11 +10,17 @@ var Cat = function() {
 		return this.catLevelArray[Math.floor(this.clickCount()/10)];
 	},this)
 
-	this.nicknames = ko.observable(["The Cutest!!", "Almost as Cute", "As cute as Unicorn Smiles", "The Cat"])
+	this.nicknames = ko.observable()
 }
 
 var ViewModel = function() {
-	this.currentCat = ko.observable( new Cat() );
+	this.currentCat = ko.observable( new Cat( {
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/tabby.jpg',
+		imgAttribution: 'http://www.flickr.com',
+		nicknames: ["The Cutest!!", "Almost as Cute", "As cute as Unicorn Smiles", "The Cat"]
+	}) );
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
