1002f36cb5b78afa1bf39f6fae8c8dab82ea6591
Sallust <daniyom@gmail.com>
2016-01-06 20:28:30 -0500

    feat: CatKnockoutFiles - changing Cat Title now included

---
+++ jsDesign/catClickerKO/js/app.js
@@ -0,0 +1,20 @@
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/tabby.jpg');
+	this.imgAttribution = ko.observable('http://www.flickr.com');
+
+	this.catLevelArray = ["infant","toddler", "pre-teen", "annoying teen", "teen", "adolescent", "adult", "dead...awkward. You totally killed it"]
+
+	this.catLevel = ko.computed(function() {
+		return this.catLevelArray[Math.floor(this.clickCount()/10)];
+	},this)
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+}
+
+ko.applyBindings(new ViewModel())
+
+//[Math.floor(this.clickCount / 10)
